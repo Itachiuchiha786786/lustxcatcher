b7ca4840ff3810e48709fa14302caa2b29c171cb
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-08-30 17:20:39 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6402009857"
     sudo_users = ["6402009857"]
-    GROUP_ID = "-1001992198513"
-    TOKEN = "7164182815:AAFxmptN2PQnqUfHt33eT6kMvnQ6cAMbGRo"
+    GROUP_ID = -1001992198513
+    TOKEN = "7271611709:AAEXRjqj1VzXH9wbKG5g1gAs7VdYO20Aia0"
     mongo_url = "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
     SUPPORT_CHAT = "+pmOzZ6tPbWxmZTg1"
     UPDATE_CHAT = "+pmOzZ6tPbWxmZTg1"
-    BOT_USERNAME = "Landlelebrobot"
-    CHARA_CHANNEL_ID = "-1002023474262"
+    BOT_USERNAME = "Seizetestbot"
+    CHARA_CHANNEL_ID = -1002023474262
     api_id = "20457610"
     api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
 
