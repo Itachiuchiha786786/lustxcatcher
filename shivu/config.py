37e5aa9f22b839fc3b0465f8f75be9d23f93c0bb
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-09-19 22:38:40 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = ["6402009857"]
     GROUP_ID = -1001992198513
     TOKEN = "7271611709:AAHfLc4cAEvk-tNFYvHniy60p2rZZt8Roe0"
-    mongo_url = "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://levi:levi@cluster0.5nctdgf.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
     SUPPORT_CHAT = "+pmOzZ6tPbWxmZTg1"
     UPDATE_CHAT = "+pmOzZ6tPbWxmZTg1"
